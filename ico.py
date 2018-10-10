86ba89044baa5881ab02e8f4362b54f5b21094d5
Cody Aldaz <craldaz@umich.edu>
2018-10-10 10:34:41 -0700

    options from default works, finished implementing union, added linear ties (not tested)

--- ico.py
+++ ico.py
@@ -9,21 +9,41 @@ class ICoord(object):
     @staticmethod
     def default_options():
         """ ICoord default options. """
+
+        if hasattr(ICoord, '_default_options'): return ICoord._default_options.copy()
+        opt = options.Options() 
         opt.add_option(
             key='isOpt',
             value=1,
+            required=False,
             allowed_types=[int],
             doc='Something to do with how coordinates are setup? Ask Paul')
 
+        opt.add_option(
+                key="mol",
+                required=False,
+                allowed_types=[pb.Molecule],
+                doc='Pybel molecule object (not OB.Mol)')
+
+        ICoord._default_options = opt
+        return ICoord._default_options.copy()
 
     @staticmethod
     def from_options(**kwargs):
         """ Returns an instance of this class with default options updated from values in kwargs"""
+        return ICoord(ICoord.default_options().set_values(kwargs))
     
-    def __init__(self,filepath):
-        self.mol=pb.readfile("xyz",filepath).next()
-        self.print_xyz()
-        self.ic_create()
+    def __init__(
+            self,
+            options,
+            ):
+        """ Constructor """
+        self.options = options
+
+        # Cache some useful attributes
+        self.mol = self.options['mol']
+        self.isOpt = self.options['isOpt']
+        #self.print_xyz()
         
     def print_xyz(self):
         for a in ob.OBMolAtomIter(self.mol.OBMol):
@@ -35,6 +55,7 @@ class ICoord(object):
         self.make_angles()
         self.make_torsions()
         self.make_imptor()
+        self.linear_ties()
         self.make_nonbond()
 
     def make_bonds(self):
@@ -67,10 +88,12 @@ class ICoord(object):
         for angle in ob.OBMolAngleIter(self.mol.OBMol):
             self.nangles+=1
             self.angles.append(angle)
+            #a is the vertex
             a=self.mol.OBMol.GetAtom(angle[0]+1)
             b=self.mol.OBMol.GetAtom(angle[1]+1)
             c=self.mol.OBMol.GetAtom(angle[2]+1)
             self.anglev.append(self.mol.OBMol.GetAngle(b,a,c))
+        #print self.angles
         #print self.anglev
         print "number of angles is %i" %self.nangles
 
@@ -172,21 +195,138 @@ class ICoord(object):
                             self.nimptor+=1
             except Exception as e: print(e)
             count+=1
-        print self.imptorv
-        print self.imptor
+            return
+
+    def update_ics(self):
+        self.update_bonds()
+        self.update_angles()
+        self.update_torsions()
+
+    def update_bonds(self):
+        self.bondd=[]
+        for bond in self.bonds:
+            a1=self.mol.OBMol.GetAtom(bond[0])
+            a2=self.mol.OBMol.GetAtom(bond[1])
+            self.bondd.append(a1.GetDistance(a2))
+
+    def update_angles(self):
+        self.anglev=[]
+        for angle in self.angles:
+            a=self.mol.OBMol.GetAtom(angle[0]+1)
+            b=self.mol.OBMol.GetAtom(angle[1]+1)
+            c=self.mol.OBMol.GetAtom(angle[2]+1)
+            # a is the vertex
+            self.anglev.append(self.mol.OBMol.GetAngle(b,a,c))
+
+    def update_torsions(self):
+        self.torv=[]
+        for torsion in self.torsions:
+            a=self.mol.OBMol.GetAtom(torsion[0]+1)
+            b=self.mol.OBMol.GetAtom(torsion[1]+1)
+            c=self.mol.OBMol.GetAtom(torsion[2]+1)
+            d=self.mol.OBMol.GetAtom(torsion[3]+1)
+            self.torv.append(self.mol.OBMol.GetTorsion(a,b,c,d))
+
+    def union_ic(
+            self,
+            icoordA,
+            icoordB,
+            ):
+        """ return the union of two lists """
+        unionBonds    = list(set(icoordA.bonds) | set(icoordB.bonds))
+        unionAngles   = list(set(icoordA.angles) | set(icoordB.angles))
+        unionTorsions = list(set(icoordA.torsions) | set(icoordB.torsions))
+        print "Saving bond union"
+        self.bonds = []
+        self.angles = []
+        self.torsions = []
+        for bond in unionBonds:
+            self.bonds.append(bond)
+        for angle in unionAngles:
+            self.angles.append(angle)
+        for torsion in unionTorsions:
+            self.torsions.append(torsion)
+
+    def bond_exists(self,bond):
+        if bond in self.bonds:
+            return True
+        else:
+            return False
 
 
-        def union_ic(
-                icoordA,
-                icoordB
-                ):
-            """ return the union of two lists """
-            unionBonds    = list(set(icoordA.bonds) | set(icoordB.bonds))
-            unionAngles   = list(set(icoordA.angles) | set(icoordB.angles))
-            unionTorsions = list(set(icoordA.torsions) | set(icoordB.torsions))
-            print "Saving bond union"
-            #for bond in unionBonds:
-                
+    def linear_ties(self):
+        maxsize=0
+        for anglev in self.anglev:
+            if anglev>160.:
+                maxsize+=1
+        print maxsize
+        #blist = np.full((3*maxsize+1,1),-1,dtype=int)
+        blist=[]
+        n=0
+        for anglev,angle in zip(self.anglev,self.angles):
+            if anglev>160.:
+                blist.append(angle)
+                print(" linear angle %i of %i: %s (%4.2f)",n+1,maxsize,angle,anglev)
+                n=+1
 
 
-ic1 = ICoord("tmp.xyz")
+        # atoms attached to linear atoms
+        clist=[[]]
+        m=np.zeros((n)) #number of nbr atoms?
+        for i in range(n):
+            # a is the vertex and not included
+            b=self.mol.OBMol.GetAtom(blist[i][1])
+            c=self.mol.OBMol.GetAtom(blist[i][2])
+            for nbr in ob.OBAtomAtomIter(b):
+                if nbr.GetIndex() != c.GetIndex():
+                    clist[i].append(nbr.GetIndex())
+                    m[i]+=1
+                    
+            for nbr in ob.OBAtomAtomIter(c):
+                if nbr.GetIndex() != b.GetIndex():
+                    clist[i].append(nbr.GetIndex())
+                    m[i]+=1
+
+        # cross linking 
+        for i in range(n):
+            a1=blist[i][1]
+            a2=blist[i][2] # not vertices
+            bond=(a1,a2)
+            if bond_exists(bond) == False:
+                print(" adding bond via linear ties %s" % bond)
+                self.bonds.append(bond)
+            for j in range(m[i]):
+                for k in range(j):
+                    b1=clist[i][j]
+                    b2=clist[i][k]
+                    found=False
+                    for angle in self.angles:
+                        if b1==angle[1] and b2==angle[2]: #0 is the vertex and don't want?
+                            found=True
+                        elif b2==angle[1] and b1==angle[2]:
+                            found=True
+                    if found==False:
+                        if bond_exists((b1,a1))==True:
+                            c1=b1
+                        if bond_exists(b2,a1)==True:
+                            c1=b2
+                        if bond_exists(b1,a2)==True:
+                            c2=b1
+                        if bond_exists(b2,a2)==True:
+                            c2=b2
+                        torsion= (c1,a1,a2,c2)
+                        print(" adding torsion via linear ties %s" %torsion)
+                        self.torsions.append(torsion)
+
+filepath="initial0000.xyz"
+mol=pb.readfile("xyz",filepath).next()
+ic1=ICoord.from_options(mol=mol)
+ic1.ic_create()
+
+#filepath="2.xyz"
+#mol=pb.readfile("xyz",filepath).next()
+#ic2=ICoord.from_options(mol=mol)
+#ic2.ic_create()
+
+#ic1.union_ic(ic1,ic2)
+#ic1.update_ics()
---
+++ options.py
@@ -0,0 +1,285 @@
+import collections
+
+class Option(object):
+
+    """ Class Option represents a key, value Option, with possible restrictions
+        on type and value, and with documentation.
+    """
+
+    def __init__(
+        self,
+        key=None,
+        value=None,
+        required=False,
+        allowed_types=None,
+        allowed_values=None,
+        doc="", 
+        ):
+
+        """ Option constructor:
+
+        Params/Members:
+            key - the string value of the key of the option.
+            value - the value of the option.
+            required - True if the user is required to specify the option,
+                False if the default value is acceptable. If required is True,
+                an error will be raised when get_value is called if value is
+                None. 
+            allowed_types - array of allowed types of value. If this field is
+                not None, a check will be performed on each call to set_value
+                and get_value to ensure that value isinstance of at least one
+                of the allowed_types.
+            allowed_values - list of allowed values of value. If this field is
+                not None, a check will be performed on each call to set_value 
+                and get_value to ensure the value is one of the allowed_values.
+            doc - string message providing helpful documentation about the
+                option.
+        """
+    
+        self.key = key
+        self.value = value
+        self.required = required
+        self.allowed_types = allowed_types
+        self.allowed_values = allowed_values
+        self.doc = doc
+
+    def get_value(self):
+
+        """ Get value for this Option and check validity.
+
+        Returns:
+            value if the Option is in a valid state, else raises RuntimeError.
+        """
+        
+        if self.required and self.value is None:
+            raise RuntimeError("Option %s is required" % self.key)
+
+        return self.value
+
+    def set_value(self, value):
+
+        """ Set value for this Option and check validity.
+
+        Result:
+            value is updated if Option is valid, else raises RuntimeError.
+        """
+
+        # Short-circuit if the value is None and the option is not required
+        if value is None and not self.required:
+            self.value = value
+            return
+
+        if self.allowed_types and not any(isinstance(value, x) for x in self.allowed_types):
+            raise RuntimeError("Option %s must be one of allowed types: %s" % (self.key, self.allowed_types))
+        if self.allowed_values and value not in self.allowed_values:
+            raise RuntimeError("Option %s must be one of allowed values: %r" % (self.key, self.allowed_values))
+
+        self.value = value
+
+    def __str__(self):
+
+        """ Return a string containing the full contents and documentation of this Option. """
+
+        s = ''
+        s += 'Option:\n'
+        s += '  Key: %s\n' % self.key
+        s += '  Value: %s\n' % self.value
+        s += '  Required: %s\n' % self.required
+        s += '  Allowed Types: %s\n' % self.allowed_types
+        s += '  Allowed Values: %s\n' % self.allowed_values
+        s += '  Doc: %s\n' % self.doc
+        s += '\n'
+        return s
+
+class Options(object):
+
+    """ Class Options represents a dict of key, value Option objects, including
+        restrictions on type, value, etc. Users should interact only with the
+        Options class - the Option class is used as internal data storage.
+
+        Generally, codes declaring and using Options objects should first
+        define the valid options, rules, and defaults by using the "add_option"
+        method. Then, when the user wishes to set or get the values of specific
+        options, a copy of the Options object should be provided for the user
+        by calling the "copy" method of Options. 
+
+        The underlying Option objects are stored in the options field, which is
+        presently a collections.OrderedDict to remember the order of Option
+        declaration. This incurs a 2x performance penalty vs. a standard dict
+        object, so we may want to optimize the performance later.
+    """
+
+    def __init__(
+        self,
+        options=None,
+        ):
+
+        """ Options constructor.
+
+        Params/Members:
+            - options - dict of key -> Option
+        """
+
+        if options is None: self.options = collections.OrderedDict()
+        else: self.options = options
+
+    def add_option(
+        self,
+        **kwargs
+        ):
+
+        """ Declare a new Option with possible default value, type and value
+            rules, and documentation.
+
+            Params: See Option constructor for valid kwargs
+            Result: Options updated with new Option corresponding to key 
+        """
+
+        self.options[kwargs['key']] = Option(
+            **kwargs
+            )
+    
+    def get_option(
+        self,
+        key,
+        ):
+
+        """ Get the Option corresponding to key (useful for doc searching an debugging).
+
+        Params:
+            - key - string key of Option (raises RuntimeError if not in Options)
+        Returns:
+            - option - the explicit Option object (most users instead want the
+              *value* of this object, which should be accessed through the
+              __getitem__ method below).
+        """
+
+        if key not in self.options:
+            raise ValueError("Key %s is not in Options" % key)
+        return self.options[key]
+
+    def __getitem__(
+        self,
+        key,
+        ):
+
+        """ Get the current value of Option corresponding to key, performing validity checks.
+
+        Params:
+            - key - string key of Option (raises RuntimeError if not in Options)
+        Returns:
+            - value - value of Option (raises RuntimeError if type, value or other validity error).
+        """
+        
+        if key not in self.options:
+            raise ValueError("Key %s is not in Options" % key)
+        return self.options[key].get_value()
+
+    def __setitem__(
+        self,
+        key,
+        value,
+        ):
+
+        """ Set the value of Option corresponding to key, performing validity checks.
+
+        Params:
+            - key - string key of Option (raises RuntimeError if not in Options)
+            - value - value of Option (raises RuntimeError if type, value or other validity error).
+        Result:
+            - Option value is updated if valid.
+        """
+
+        if key not in self.options:
+            raise ValueError("Key %s is not in Options" % key)
+        return self.options[key].set_value(value)
+
+    def set_values(
+        self,
+        options,
+        ):
+
+        """ Set the values of multiple options. 
+
+        Params:
+            - options - dict of key, value pairs to set (calls __setitem__ once
+              per key, value pair).
+        Results:
+            - Option values are updated if valid.
+        """
+        
+        for k, v in options.iteritems():
+            self[k] = v
+        return self
+
+    def copy(self): 
+
+        """ Return a 1-level shallow copy of this Options object. This makes
+            copies of all underlying Option objects so that changes to the new
+            Options object will not affect the original Options object.
+        """
+
+        options2 = collections.OrderedDict()
+        for k, v in self.options.iteritems():
+            options2[k] = Option(**v.__dict__)
+        return Options(options=options2)
+
+    def __str__(self):
+
+        """ Return the string representations of all Option objects in this Options, in insertion order. """
+    
+        s = ''.join(str(v) for v in self.options.values())
+        return s
+        
+if __name__ == '__main__':
+
+    import time
+
+    start = time.time()
+    options1 = Options()        
+    for k in range(500):
+        options1.add_option(
+            key='size%d' % k,
+            value=0,
+            allowed_types=[int],
+            allowed_values=[0,1],
+            ) 
+    start = time.time()
+    options2 = options1.copy()
+    print '%11.3E' % (time.time() - start)
+
+    start = time.time()
+    options3 = Options()        
+    options3.add_option(
+        key='size',
+        value=0,
+        allowed_types=[int],
+        allowed_values=[0,1],
+        ) 
+    options4 = options3.copy()
+    print '%11.3E' % (time.time() - start)
+
+    start = time.time()
+    options3 = Options()        
+    options3.add_option(
+        key='size',
+        value=0,
+        allowed_types=[int],
+        allowed_values=[0,1],
+        ) 
+    options4 = options3.copy()
+    print '%11.3E' % (time.time() - start)
+
+    start = time.time()
+    options3 = Options()        
+    options3.add_option(
+        key='size',
+        value=0,
+        allowed_types=[int],
+        allowed_values=[0,1],
+        ) 
+    options4 = options3.copy()
+    print '%11.3E' % (time.time() - start)
+
+    options4.set_values({ 'size' : 1 })
+    print options4
