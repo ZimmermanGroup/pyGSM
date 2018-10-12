434b85fc30d0c850ffb6072f6f952f97b0b1a322
Cody Aldaz <craldaz@umich.edu>
2018-10-12 10:29:14 -0700

    Added vdw_radii class

---
+++ penalty_pes.py
@@ -0,0 +1,80 @@
+import options
+
+class Base(object):
+    """ Base object for potential energy surface calculators """
+
+    @staticmethod
+    def default_options():
+        """ Base default options. """
+
+        if hasattr(Base, '_default_options'): return Base._default_options.copy()
+        opt = options.Options() 
+
+        opt.add_option(
+            key='calc_states',
+            value=(0,0),
+            required=True,
+            allowed_types=[list],
+            doc='')
+
+        opt.add_option(
+                key='nocc',
+                value=0,
+                required=False,
+                allowed_types=[int],
+                doc='number of occupied orbitals (for CAS)')
+
+        opt.add_option(
+                key='nactive',
+                value=0,
+                required=False,
+                allowed_types=[int],
+                doc='number of active orbitals (for CAS)')
+
+        opt.add_option(
+                key='nstates',
+                value=1,
+                required=False,
+                allowed_types=[int],
+                doc='Number of states')
+
+        opt.add_option(
+                key='basis',
+                value=0,
+                required=False,
+                allowed_types=[str],
+                doc='Basis set')
+
+        opt.add_option(
+                key='filepath',
+                value='initial0000.xyz',
+                required=False,
+                allowed_types=[str],
+                doc='path to xyz file')
+
+        Base._default_options = opt
+        return Base._default_options.copy()
+
+    def __init__(self,
+            options,
+            ):
+        """ Constructor """
+        self.options = options
+        # Cache some useful atributes
+        self.calc_states=self.options['calc_states']
+        self.nstates=self.options['nstates']
+        self.filepath = self.options['filepath']
+        self.nocc=self.options['nocc']
+        self.nactive=self.options['nactive']
+        self.basis=self.options['basis']
+
+    def getEnergy(self):
+        raise NotImplementedError()
+
+    def getGrad(self):
+        raise NotImplementedError()
+
+    def finite_difference(self):
+        self.getEnergy() 
+        print("Not yet implemented")
+        return 0
---
+++ vdw_radii.py
@@ -0,0 +1,10 @@
+#TODO These need to be filled in!
+
+radii = {
+        1: 1.3,
+        6: 1.67,
+        7: 1.66,
+        8: 1.65,
+        9: 1.6,
+        }
+
