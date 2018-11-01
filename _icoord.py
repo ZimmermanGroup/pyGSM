add buffered print to icoord, penalty seems to work for molpro, fix scale gradient.

_icoord.py
@@ -264,7 +264,7 @@ class Mixin:
         dEpre = np.dot(np.transpose(dq0),self.gradq) + 0.5*np.dot(np.transpose(dEtemp),dq0)
         dEpre *=KCAL_MOL_PER_AU
         if abs(dEpre)<0.005: dEpre = np.sign(dEpre)*0.005
-        print( "predE: %1.4f " % dEpre) 
+        print( "predE: %1.4f " % dEpre),
         return dEpre
 
     def grad_to_q(self,grad):

icoord.py
@@ -8,6 +8,7 @@ from units import *
 import itertools
 from copy import deepcopy
 import manage_xyz
+import StringIO 
 
 from _icoord import Mixin
 np.set_printoptions(precision=4)
@@ -27,6 +28,13 @@ class ICoord(Mixin):
             required=False,
             allowed_types=[int],
             doc='Something to do with how coordinates are setup? Ask Paul')
+
+        opt.add_option(
+            key='print_level',
+            value=1,
+            required=False,
+            allowed_types=[int],
+            doc='0-- no printing, 1-- printing')
 
         opt.add_option(
             key='MAX_FRAG_DIST',
@@ -145,6 +153,7 @@ class ICoord(Mixin):
         self.resetopt = self.options['resetopt']
         self.angles = self.options['angles']
         self.torsions = self.options['torsions']
+        self.print_level = self.options['print_level']
 
         #self.print_xyz()
         self.Elements = elements.ElementData()
@@ -165,7 +174,7 @@ class ICoord(Mixin):
             self.ixflag = 0
             self.energy = 0.
             self.DMAX = 0.1
-            self.DMIN0 =self.DMAX/100.
+            self.DMIN0 =self.DMAX/10.
             self.coords = np.zeros((len(self.mol.atoms),3))
             for i,a in enumerate(ob.OBMolAtomIter(self.mol.OBMol)):
                 self.coords[i,0] = a.GetX()
@@ -1053,11 +1062,16 @@ class ICoord(Mixin):
         energies=[]
         Es =[]
         self.do_bfgs=False # gets reset after each step
+        self.buf = StringIO.StringIO()
 
         print "Initial energy is %1.4f\n" % self.V0
+        self.buf.write("\n Writing convergence:")
+
 
         for step in range(nsteps):
-            print("\niteration step %i" %step)
+            if self.print_level==1:
+                print("\nOpt step: %i" %(step+1)),
+            self.buf.write("\nOpt step: %d" %(step+1))
 
             # => Opt step <= #
             smag =self.opt_step(nconstraints)
@@ -1088,6 +1102,7 @@ class ICoord(Mixin):
 
             if self.gradrms<self.OPTTHRESH:
                 break
+        print(self.buf.getvalue())
         print "Final energy is %2.5f" % (self.V0 + self.energy)
         return smag
 
@@ -1107,7 +1122,9 @@ class ICoord(Mixin):
         self.gradq = self.grad_to_q(grad)
         pgradrms = self.gradrms
         self.gradrms = np.linalg.norm(self.gradq)*1./np.sqrt(self.nicd)
-        print("gradrms = %1.5f" % self.gradrms),
+        if self.print_level==1:
+            print("gradrms = %1.5f" % self.gradrms),
+        self.buf.write(" gRMS=%1.5f" %(self.gradrms))
         if self.gradrms<self.OPTTHRESH:
             return
 
@@ -1125,7 +1142,9 @@ class ICoord(Mixin):
 
         # regulate max overall step
         smag = np.linalg.norm(dq)
-        print(" ss: %1.5f (DMAX: %1.3f)" %(smag,self.DMAX)),
+        self.buf.write(" ss: %1.5f (DMAX: %1.3f" %(smag,self.DMAX))
+        if self.print_level==1:
+            print(" ss: %1.5f (DMAX: %1.3f)" %(smag,self.DMAX)),
 
         if smag>self.DMAX:
             dq = np.fromiter(( xi*self.DMAX/smag for xi in dq), dq.dtype)
@@ -1151,18 +1170,24 @@ class ICoord(Mixin):
      
         # => calc energyat new position <= #
         self.energy = self.PES.get_energy(self.geom) - self.V0
-        print "E is %4.5f" % self.energy,
+        self.buf.write(" E(M): %4.5f" %(self.energy))
+        if self.print_level==1:
+            print "E(M): %4.5f" % self.energy,
 
         # check goodness of step
         dEstep = self.energy - energyp
         dEpre = self.compute_predE(dq)
 
         ratio = dEstep/dEpre
-        print "ratio is %1.4f" % ratio,
+        self.buf.write(" ratio: %1.4f" %(ratio))
+        if self.print_level==1:
+            print "ratio is %1.4f" % ratio,
 
         # => step controller  <= #
         if dEstep>0.01:
-            print("decreasing DMAX"),
+            if self.print_level==1:
+                print("decreasing DMAX"),
+            self.buf.write(" decreasing DMAX")
             if smag <self.DMAX:
                 self.DMAX = smag/1.5
             else: 
@@ -1178,19 +1203,24 @@ class ICoord(Mixin):
                 self.Hintp_to_Hint()
                 self.do_bfgs=False
         elif ratio<0.25:
-            print("decreasing DMAX"),
+            if self.print_level==1:
+                print("decreasing DMAX"),
+            self.buf.write(" decreasing DMAX")
             if smag<self.DMAX:
                 self.DMAX = smag/1.1
             else:
                 self.DMAX = self.DMAX/1.2
             self.make_Hint()
         elif (ratio>0.75 and ratio<1.25) and smag > self.DMAX and self.gradrms<pgradrms*1.35:
-            print("increasing DMAX"),
+            if self.print_level==1:
+                print("increasing DMAX"),
+            self.buf.write(" increasing DMAX")
             self.DMAX=self.DMAX*1.1 + 0.01
             if self.DMAX>0.25:
                 self.DMAX=0.25
         if self.DMAX<self.DMIN0:
             self.DMAX=self.DMIN0
+
         return  smag
 
 

molpro.py
@@ -102,11 +102,11 @@ class Molpro(Base):
                     for i in range(len(geom)):
                         findline = next(f,'').strip()
                         mobj = re.match(r'^\s*(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s*$', findline)
-                        tmpgrad.append((
+                        tmpgrad.append([
                             float(mobj.group(2)),
                             float(mobj.group(3)),
                             float(mobj.group(4)),
-                            ))
+                            ])
                     tmpgrada.append(tmpgrad)
                     tmpgrad = []
 
@@ -135,15 +135,12 @@ class Molpro(Base):
 
     def getgrad(self,state,multiplicity):
         tmp = self.search_tuple(self.grada,multiplicity)
-        return tmp[state][1]
+        return np.asarray(tmp[state][1])*ANGSTROM_TO_AU
 
     def get_gradient(self,geom,multiplicity,state):
         if self.hasRanForCurrentCoords==False:
             self.run(geom)
         return self.getgrad(state,multiplicity)
-
-
-    #    return np.asarray(grad)*ANGSTROM_TO_AU
 
 
     @staticmethod
@@ -166,6 +163,6 @@ if __name__ == '__main__':
     e=lot.get_energy(geom,3,0)
     print e
     g=lot.get_gradient(geom,1,0)
-    #print g
-    #g=lot.get_gradient(geom,3,0)
+    print g
+    g=lot.get_gradient(geom,3,0)
     #print g

penalty_pes.py
@@ -18,41 +18,21 @@ class Penalty_PES(PES):
         """ Returns an instance of this class with default options updated from values in kwargs"""
         return Penalty_PES(Penalty_PES.default_options().set_values(kwargs))
 
-    #def getGrad(self):
-    #    avg_grad = self.PES.getGrad() 
-    #    dgrad = self.PES.grada[1] - self.PES.grada[0]
-    #    dgrad = dgrad.reshape((3*len(self.coords),1))
-
-    #    prefactor = (self.dE**2. + 2.*self.alpha*self.dE)/((self.dE + self.alpha)**2.)
-    #    #print prefactor
-    #    #print self.PES.grada[0].flatten()
-    #    #print self.PES.grada[1].flatten()
-    #    #print avg_grad.T
-    #    tmp = dgrad*prefactor*self.sigma
-    #    #print prefactor
-    #    print tmp.T
-    #    print avg_grad.T
-    #    grad = avg_grad + tmp
-    #    print grad.T
-
-    #    return grad
-
     def get_energy(self,geom):
         avgE = 0.5*(self.PES1.get_energy(geom) + self.PES2.get_energy(geom))
         self.dE = self.PES2.get_energy(geom) - self.PES1.get_energy(geom)
+        print "delta E = %1.4f" %self.dE,
         #TODO what to do if PES2 is or goes lower than PES1?
         G = (self.dE**2.)/(self.dE + self.alpha)
+        #print "G = %1.4f" % G,
         return avgE+self.sigma*G
 
     def get_gradient(self,geom):
-        #print self.PES1.get_gradient(geom)
-        #print np.shape( self.PES1.get_gradient(geom))
         avg_grad = 0.5*(self.PES1.get_gradient(geom) + self.PES2.get_gradient(geom))
-        #print avg_grad
         dgrad = self.PES2.get_gradient(geom) - self.PES1.get_gradient(geom)
-        #print dgrad
-        factor = (self.dE**2. + 2.*self.alpha*self.dE)/((self.dE + self.alpha)**2.)
-        return avg_grad + self.sigma*factor*dgrad
+        factor = self.sigma*(self.dE**2. + 2.*self.alpha*self.dE)/((self.dE + self.alpha)**2.)
+        #print "factor is %1.4f" % factor
+        return avg_grad + factor*dgrad
 
 
 if __name__ == '__main__':
@@ -97,7 +77,7 @@ if __name__ == '__main__':
         geom=manage_xyz.read_xyz(filepath,scale=1)   
         nocc=7
         nactive=2
-        lot=Molpro.from_options(states=[(1,0),(1,1)],nocc=nocc,nactive=nactive,basis='6-31gs')
+        lot=Molpro.from_options(states=[(1,0),(1,1)],nocc=nocc,nactive=nactive,basis='6-31g*',nproc=4)
         pes1 = PES.from_options(lot=lot,ad_idx=0,multiplicity=1)
         pes2 = PES.from_options(lot=lot,ad_idx=1,multiplicity=1)
         #p = Penalty_PES(pes1,pes2)
@@ -107,13 +87,12 @@ if __name__ == '__main__':
             }))
 
         #e =p.get_energy(geom)
-        #print e
-        #g = p.get_gradient(geom)
+        #g =p.get_gradient(geom)
+        #print e 
         #print g
-
         mol=pb.readfile("xyz",filepath).next()
         mol.OBMol.AddBond(6,1,1)
         print "####### ic1 ##########"
         ic1=ic.ICoord.from_options(mol=mol,PES=p,resetopt=False)
-        ic1.optimize(50)
+        ic1.optimize(100)
 

qchem.py
@@ -91,7 +91,8 @@ class QChem(Base):
 
     def getE(self,state,multiplicity):
         tmp = self.search_tuple(self.E,multiplicity)
-        return tmp[state][1]*KCAL_MOL_PER_AU
+        return np.asarray(tmp[state][1])*ANGSTROM_TO_AU
+
 
     def get_gradient(self,geom,multiplicity,state):
         if self.has_nelectrons==False:
@@ -104,7 +105,7 @@ class QChem(Base):
 
     def getgrad(self,state,multiplicity):
         tmp = self.search_tuple(self.grada,multiplicity)
-        return tmp[state][1]
+        return tmp[state][1]*ANGSTROM_TO_AU
 
     @staticmethod
     def from_options(**kwargs):