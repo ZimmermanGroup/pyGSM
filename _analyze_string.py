f591db405016a3fa13d2aeeded4ab418d6ef2cda
Cody Aldaz <craldaz@umich.edu>
2018-11-15 12:38:11 -0800

    forgot to add _analyze_string. added more functions to analyze_string.

---
+++ _analyze_string.py
@@ -0,0 +1,187 @@
+import numpy as np
+
+class Analyze:
+    def find_peaks(self,rtype):
+        #rtype 1: growing
+        #rtype 2: opting
+        #rtype 3: intermediate check
+        if rtype==1:
+            nnodes=self.nR
+        elif rtype==2 or rtype==3:
+            nnodes=self.nnodes
+        else:
+            raise ValueError("find peaks bad input")
+        #if rtype==1 or rtype==2:
+        #    print "Energy"
+        #    print self.energies
+        alluptol=0.1
+        alluptol2=0.5
+        allup=True
+        diss=False
+        for n in range(len(self.energies[:nnodes])):
+            if self.energies[n]+alluptol<self.energies[n-1]:
+                allup=False
+                break
+
+        if self.energies[nnodes-1]>15.0:
+            if nnodes-3>0:
+                if (abs(self.energies[nnodes-1]-self.energies[nnodes-2])<alluptol2 and
+                abs(self.energies[nnodes-2]-self.energies[nnodes-3])<alluptol2 and
+                abs(self.energies[nnodes-3]-self.energies[nnodes-4])<alluptol2):
+                    print "possible dissociative profile"
+                    diss=True
+
+        print "nnodes ",nnodes  
+        print "allup? ",allup
+        npeaks1=0
+        npeaks2=0
+        minnodes=[]
+        maxnodes=[]
+        if self.energies[1]>self.energies[0]:
+            minnodes.append(0)
+        for n in range(self.n0,nnodes):
+            if self.energies[n+1]>self.energies[n]:
+                if self.energies[n]<self.energies[n-1]:
+                    minnodes.append(n)
+            if self.energies[n+1]<self.energies[n]:
+                if self.energies[n]>self.energies[n-1]:
+                    maxnodes.append(n)
+
+        print "min nodes"
+        print minnodes
+        print "max nodes" 
+        print maxnodes
+        npeaks1 = len(maxnodes)
+        print "number of peaks is ",npeaks1
+        ediff=0.5
+        PEAK4_EDIFF = 2.0
+        if rtype==1:
+            ediff=1.
+        if rtype==3:
+            ediff=PEAK4_EDIFF
+
+        if rtype==1:
+            nmax = np.argmax(self.energies[:self.nR])
+            emax = float(max(self.energies[:self.nR]))
+        else:
+            emax = float(max(self.energies))
+            nmax = np.argmax(self.energies)
+
+        print "emax and nmax in find peaks %3.4f,%i " % (emax,nmax)
+
+        #check if any node after peak is less than 2 kcal below
+        for n in maxnodes:
+            diffs=( self.energies[n]-e>ediff for e in self.energies[n:nnodes])
+            if any(diffs):
+                found=n
+                npeaks2+=1
+        npeaks = npeaks2
+        print "found %i significant peak(s) TOL %3.2f" %(npeaks,ediff)
+
+        #handle dissociative case
+        if rtype==3 and npeaks==1:
+            nextmin=0
+            for n in range(found,numnodes-1):
+                if n in minnodes:
+                    nextmin=n
+                    break
+            if nextmin:
+                npeaks=2
+
+        if rtype==3:
+            return num_nodes
+        if allup==True and npeaks==0:
+            return -1
+        if diss==True and npeaks==0:
+            return -2
+
+        return npeaks
+
+    def past_ts(self):
+        ispast=ispast1=ispast2=ispast3=0
+        THRESH1=5.
+        THRESH2=3.
+        THRESH3=-1.
+        THRESHB=0.05
+        CTHRESH=0.005
+        OTHRESH=-0.015
+        emax = -100.
+        nodemax =1
+        #n0 is zero until after finished growing
+        ns = self.n0-1
+        if ns<nodemax: ns=nodemax
+
+        print "Energies"
+        for n in range(ns,self.nR):
+            print(" %4.3f" % self.energies[n])
+            if self.energies[n]>emax:
+                nodemax=n
+                emax=self.energies[n]
+        print "nodemax ",nodemax
+
+        for n in range(nodemax,self.nR):
+            if self.energies[n]<emax-THRESH1:
+                ispast1+=1
+            if self.energies[n]<emax-THRESH2:
+                ispast2+=1
+            if self.energies[n]<emax-THRESH3:
+                ispast3+=1
+            if ispast1>1:
+                break
+        print "ispast1",ispast1
+        print "ispast2",ispast2
+        print "ispast3",ispast3
+
+        cgrad = self.icoords[self.nR-1].gradq[self.icoords[self.nR-1].nicd-1]
+        print(" cgrad: %4.3f nodemax: %i nR: %i" %(cgrad,nodemax,self.nR))
+
+        if cgrad>CTHRESH:
+            print "constraint gradient positive"
+            ispast=2
+        elif ispast1>0 and cgrad>OTHRESH:
+            print "over the hill(1)"
+            ispast=1
+        elif ispast2>1:
+            print "over the hill(2)"
+            ispast=1
+        else:
+            ispast=0
+
+        if ispast==0:
+            bch=self.check_for_reaction_g(1)
+            if ispast3>1 and bch:
+                print "over the hill(3) %i connection changed" %bch
+                ispast=3
+        print "ispast=",ispast
+        return ispast
+
+    def check_for_reaction_g(self,rtype):
+        nadds = self.driving_coords.count("ADD")
+        nbreaks = self.driving_coords.count("BREAK")
+        if (nadds+nbreaks) <1:
+            return 0
+        nadded=0
+        nbroken=0
+        for i in self.driving_coords:
+            if "ADD" in i:
+                bond=(i[1],i[2])
+                d= self.icoords[nnR-1].distance(bond[0],bond[1])
+                d0 = (self.icoords[nnR-1].get_element_VDW(bond[0]) +
+                        ICoord1.get_element_VDW(bond[1]))/2.
+                if d<d0:
+                    nadded+=1
+            if "BREAK" in i:
+                bond=(i[1],i[2])
+                d= self.icoords[nnR-1].distance(bond[0],bond[1])
+                d0 = (self.icoords[nnR-1].get_element_VDW(bond[0]) +
+                        ICoord1.get_element_VDW(bond[1]))/2.
+                if d>d0:
+                    nbroken+=1
+        if rtype==1:
+            if (nadded+nbroken)>(nadds+nbreaks):
+                isrxn=nadded+nbroken
+        else:
+            isrxn=nadded+nbroken
+        print "check_for_reaction_g isrxn: %i nadd+nbrk: %i" %(isrxn,nadds+nbreaks)
+        return isrxn
+
