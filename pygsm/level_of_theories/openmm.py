eb7bb0dcdf8a269b8673e45d389a86b95ff65d8f
Cody Aldaz <craldaz@umich.edu>
2020-08-14 16:17:10 -0700

    OpenMM can use torsion and translational Harmonic restraints.

--- pygsm/level_of_theories/openmm.py
+++ pygsm/level_of_theories/openmm.py
@@ -19,6 +19,7 @@ try:
 except:
     from base_lot import Lot
 from utilities import *
+from coordinate_systems import Dihedral
 
 class OpenMM(Lot):
     def __init__(self,options):
@@ -26,16 +27,22 @@ class OpenMM(Lot):
         super(OpenMM,self).__init__(options)
 
         # get simulation from options if it exists
-        self.options['job_data']['simulation'] = self.options['job_data'].get('simulation',None)
+        #self.options['job_data']['simulation'] = self.options['job_data'].get('simulation',None)
+        self.simulation = self.options['job_data'].get('simulation',None)
 
         if self.lot_inp_file is not None and self.simulation is None:
 
             # Now go through the logic of determining which FILE options are activated.
             self.file_options.set_active('use_crystal',False,bool,"Use crystal unit parameters")
-            self.file_options.set_active('use_pme',False,bool,"Use particle mesh ewald-- requires periodic boundary conditions")
-            self.file_options.set_active('cutoff',1.0,float,depend=(self.file_options.use_pme),msg="Requires PME")
+            self.file_options.set_active('use_pme',False,bool,'',"Use particle mesh ewald-- requires periodic boundary conditions")
+            self.file_options.set_active('cutoff',1.0,float,'',depend=(self.file_options.use_pme),msg="Requires PME")
             self.file_options.set_active('prmtopfile',None,str,"parameter file")
             self.file_options.set_active('inpcrdfile',None,str,"inpcrd file")
+            self.file_options.set_active('restrain_torfile',None,str,"list of torsions to restrain")
+            self.file_options.set_active('restrain_tranfile',None,str,"list of translations to restrain")
+
+            for line in self.file_options.record():
+                print(line)
 
             # set all active values to self for easy access
             for key in self.file_options.ActiveOptions:
@@ -44,37 +51,80 @@ class OpenMM(Lot):
             nifty.printcool(" Options for OpenMM")
             for val in [self.prmtopfile,self.inpcrdfile]:
                 assert val!=None,"Missing prmtop or inpcrdfile"
-            for line in self.record():
-                print(line)
 
             # Integrator will never be used (Simulation requires one)
             integrator = openmm.VerletIntegrator(1.0)
 
             # create simulation object
-            if self.use_crystal=='yes':
-                crystal = load_file(prmtopfile,inpcrdfile)
-                if self.use_pme=='yes':
+            if self.use_crystal:
+                crystal = load_file(self.prmtopfile,self.inpcrdfile)
+                if self.use_pme:
                     system = crystal.createSystem(
                         nonbondedMethod=openmm_app.PME,
-                        nonbondedCutoff=cutoff*openmm_units.nanometer,
+                        nonbondedCutoff=self.cutoff*openmm_units.nanometer,
                         )
                 else:
                     system = crystal.createSystem(
                         nonbondedMethod=openmm_app.NoCutoff,
                         )
+
+                # Torsion restraint
+                if self.restrain_torfile is not None:
+                    nifty.printcool(" Adding torsional restraints!")
+
+                    # Harmonic constraint
+                    tforce = openmm.CustomTorsionForce("0.5*k*min(dtheta, 2*pi-dtheta)^2; dtheta = abs(theta-theta0); pi = 3.1415926535")
+                    tforce.addPerTorsionParameter("k")
+                    tforce.addPerTorsionParameter("theta0")
+                    system.addForce(tforce)
+                    
+                    xyz = manage_xyz.xyz_to_np(self.geom)
+                    with open(self.restrain_torfile,'r') as input_file:
+                        for line in input_file:
+                            columns = line.split()
+                            a = int(columns[0])
+                            b = int(columns[1])
+                            c = int(columns[2])
+                            d = int(columns[3])
+                            k = float(columns[4])
+                            dih = Dihedral(a,b,c,d)
+                            theta0 = dih.value(xyz)
+                            tforce.addTorsion(a,b,c,d,[k,theta0])
+
+                # Translation restraint
+                if self.restrain_tranfile is not None:
+                    nifty.printcool(" Adding translational restraints!")
+                    trforce = openmm.CustomExternalForce("k*periodicdistance(x, y, z, x0, y0, z0)^2")
+                    trforce.addPerParticleParameter("k")
+                    trforce.addPerParticleParameter("x0")
+                    trforce.addPerParticleParameter("y0")
+                    trforce.addPerParticleParameter("z0")
+                    system.addForce(trforce)
+
+                    xyz = manage_xyz.xyz_to_np(self.geom)
+                    with open(self.restrain_tranfile,'r') as input_file:
+                        for line in input_file:
+                            columns = line.split()
+                            a = int(columns[0])
+                            k = float(columns[1])
+                            x0=xyz[a,0]*0.1  # Units are in nm 
+                            y0=xyz[a,1]*0.1  # Units are in nm 
+                            z0=xyz[a,2]*0.1  # Units are in nm 
+                            trforce.addParticle(a,[k,x0,y0,z0])
+
                 self.simulation = openmm_app.Simulation(crystal.topology, system, integrator)
                 # set the box vectors
-                inpcrd = openmm_app.AmberInpcrdFile(inpcrdfile)
+                inpcrd = openmm_app.AmberInpcrdFile(self.inpcrdfile)
                 if inpcrd.boxVectors is not None:
                     print(" setting box vectors")
                     print(inpcrd.boxVectors)
                     self.simulation.context.setPeriodicBoxVectors(*inpcrd.boxVectors)
             else:  # Do not use crystal parameters
-                prmtop = openmm_app.AmberPrmtopFile(prmtopfile)
-                if use_pme=='yes':
+                prmtop = openmm_app.AmberPrmtopFile(self.prmtopfile)
+                if self.use_pme:
                     system = prmtop.createSystem(
                         nonbondedMethod=openmm_app.PME,
-                        nonbondedCutoff=cutoff*openmm_units.nanometer,
+                        nonbondedCutoff=self.cutoff*openmm_units.nanometer,
                         )
                 else:
                     system = prmtop.createSystem(
