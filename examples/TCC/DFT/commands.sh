55870c024f4d9ec1f12d1f34ce59f3d2d96b4cb7
Cody Aldaz <craldaz@stanford.edu>
2021-02-17 12:13:20 -0800

    Remove junk examples.

--- examples/TCC/DFT/commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#This file was created on 06/06/2019
-
-#module load pygsm
-#module load tcc
-#if load pygsm simply call gsm
-
-python ../../../pygsm/wrappers/main.py -xyzfile ../../../data/diels_alder.xyz -mode DE_GSM -package TeraChemCloud -lot_inp_file tcc_lot_inp_file.txt > log &
