Feature: Node coordinates can now be calculated in parallel, also fixed an error in the calculation of reaction paths with  large rotations

pygsm/coordinate_systems/delocalized_coordinates.py
@@ -379,7 +379,7 @@ class DelocalizedInternalCoordinates(InternalCoordinates):
             self.Vecs = block_matrix.project_constraint(self.Vecs,cVecs)
             #print(" shape of DLC")
             #print(self.Vecs.shape)
-        return
+        return self.Vecs
 
 
     def build_dlc_conjugate(self, xyz, C=None):
@@ -863,12 +863,12 @@ class DelocalizedInternalCoordinates(InternalCoordinates):
 
     def GInverse_EIG(self, xyz):
         xyz = xyz.reshape(-1,3)
-        nifty.click()
+        #nifty.click()
         G = self.GMatrix(xyz)
-        time_G = nifty.click()
+        #time_G = nifty.click()
         #Gi = np.linalg.inv(G)
         tmpGi = [ np.linalg.inv(g) for g in G.matlist ]
-        time_inv = nifty.click()
+        #time_inv = nifty.click()
         #print("G-time: %.3f Inv-time: %.3f" % (time_G, time_inv))
         return block_matrix(tmpGi)
 

pygsm/coordinate_systems/primitive_internals.py
@@ -76,11 +76,11 @@ class PrimitiveInternalCoordinates(InternalCoordinates):
             for g in self.fragments: g.__class__ = MyG
 
             self.get_hybrid_indices(xyz)
-            nifty.click()
+            #nifty.click()
             self.newMakePrimitives(xyz)
             print(" done making primitives")
-            time_build = nifty.click()
-            print(" make prim %.3f" % time_build)
+            #time_build = nifty.click()
+            #print(" make prim %.3f" % time_build)
 
         # Reorder primitives for checking with cc's code in TC.
         # Note that reorderPrimitives() _must_ be updated with each new InternalCoordinate class written.
@@ -360,12 +360,12 @@ class PrimitiveInternalCoordinates(InternalCoordinates):
     def GInverse_SVD(self, xyz):
         xyz = xyz.reshape(-1,3)
         # Perform singular value decomposition
-        nifty.click()
+        #nifty.click()
         loops = 0
         while True:
             try:
                 G = self.GMatrix(xyz)
-                time_G = nifty.click()
+                #time_G = nifty.click()
                 start=0
                 tmpUvecs=[]
                 tmpVvecs=[]
@@ -378,7 +378,7 @@ class PrimitiveInternalCoordinates(InternalCoordinates):
                 V = block_matrix(tmpVvecs)
                 UT = block_matrix(tmpUvecs)
                 S = block_matrix(tmpSvecs)
-                time_svd = nifty.click()
+                #time_svd = nifty.click()
             except np.linalg.LinAlgError:
                 logger.warning("\x1b[1;91m SVD fails, perturbing coordinates and trying again\x1b[0m")
                 xyz = xyz + 1e-2*np.random.random(xyz.shape)
@@ -411,16 +411,16 @@ class PrimitiveInternalCoordinates(InternalCoordinates):
 
     def GInverse_EIG(self, xyz):
         xyz = xyz.reshape(-1,3)
-        nifty.click()
+        #nifty.click()
         G = self.GMatrix(xyz)
-        time_G = nifty.click()
+        #time_G = nifty.click()
 
         matlist=[]
         for Gmat in G.matlist:
             matlist.append(np.linalg.inv(Gmat))
         
         Gt = block_matrix(matlist)
-        time_inv = nifty.click()
+        #time_inv = nifty.click()
         #print("G-time: %.3f Inv-time: %.3f" % (time_G, time_inv))
 
         return Gt

pygsm/wrappers/molecule.py
@@ -383,6 +383,7 @@ class Molecule(object):
 
     @property
     def mass_weighted_cartesians(self):
+        M = self.total_mass_au
         return np.asarray( [self.xyz[i,:]*self.atomic_mass[i]/M for i in range(self.natoms)] )
 
     @property
@@ -504,13 +505,18 @@ class Molecule(object):
         if self.frozen_atoms is not None:
             return len(self.frozen_atoms)
         else:
-            0
+            return 0
 
     def update_xyz(self,dq=None,verbose=True):
         #print " updating xyz"
         if dq is not None:
             self.xyz = self.coord_obj.newCartesian(self.xyz,dq,frozen_atoms=self.frozen_atoms,verbose=verbose)
         return self.xyz
+
+    def update_and_move(self,tan,deltadq,verbose=True):
+        self.update_coordinate_basis(tan)
+        dq = deltadq*self.constraint[:,0]
+        self.update_xyz(dq,verbose=verbose)
 
     def update_MW_xyz(self,mass,dq,verbose=True):
         self.xyz = self.coord_obj.massweighted_newCartesian(self.xyz,dq,mass,verbose)
@@ -623,6 +629,12 @@ class Molecule(object):
     @property
     def node_id(self):
         return self.Data['node_id']
+
+    
+    @node_id.setter
+    def node_id(self,value):
+        self.Data['node_id'] = value
+        self.PES.lot.node_id = value
 
 if __name__=='__main__':
     from level_of_theories import Molpro